f5f1ce75b6832f0ce2991153c90133d4c4a198f6
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-07-23 14:44:55 +0600

    firebase authentication done and added a footer section

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,18 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCIlwJn5n6Y4BK87bLeOQGDH_7VnR9UzAM",
-  authDomain: "collegiate-nexus.firebaseapp.com",
-  projectId: "collegiate-nexus",
-  storageBucket: "collegiate-nexus.appspot.com",
-  messagingSenderId: "235640702328",
-  appId: "1:235640702328:web:26c2d87314d5508426748d"
+  apiKey: "AIzaSyA2izK0HQWgsiJzWHDA6U8H-xpOhPYI3tM",
+  authDomain: "college-bd-c8e6a.firebaseapp.com",
+  projectId: "college-bd-c8e6a",
+  storageBucket: "college-bd-c8e6a.appspot.com",
+  messagingSenderId: "243838170365",
+  appId: "1:243838170365:web:01bca08bd614baec96fa94"
 };
+const app = initializeApp(firebaseConfig);
+  
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 export default app
--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Pages/Home/Home/NavBar";
 import{ Toaster } from 'react-hot-toast';
+import Footer from "../Pages/Home/Home/Footer";
 
 const Main = () => {
     return (
         <div>
             <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
             <Toaster></Toaster>
             
         </div>
